0ab5da9db42725293bf5b50fcf95a1088811b9c0
gratajczak <grzegorz.ratajczak@kinguin.net>
2026-02-10 16:30:07 +0100

    add cookie banner

--- src/app/ssr/page.tsx
+++ src/app/ssr/page.tsx
@@ -1,17 +1,26 @@
 import { PerformanceMonitor } from "@/components/performance-monitor";
 import { getSectionServerContent } from "@/components/sections/section-server";
+import Script from "next/script";
 
 export default async function SSRPage() {
-  const { element, serverLoadTime, serverApiCallCount } =
-    await getSectionServerContent();
+    const { element, serverLoadTime, serverApiCallCount } =
+        await getSectionServerContent();
 
-  return (
-    <>
-      {element}
-      <PerformanceMonitor
-        serverLoadTimes={{ ssr: serverLoadTime }}
-        serverApiCalls={serverApiCallCount}
-      />
-    </>
-  );
+    return (
+        <>
+            <Script
+                id="Cookiebot"
+                src="https://consent.cookiebot.com/uc.js"
+                data-cbid="45060aa1-a02e-4da1-b894-ba61862915bc"
+                data-blockingmode="auto"
+                strategy="lazyOnload"
+                type="text/javascript"
+            />
+            {element}
+            <PerformanceMonitor
+                serverLoadTimes={{ ssr: serverLoadTime }}
+                serverApiCalls={serverApiCallCount}
+            />
+        </>
+    );
 }
