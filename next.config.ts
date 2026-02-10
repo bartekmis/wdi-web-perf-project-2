fab95f0904fd89941b909b80f298c6cc9ee9d8bc
Mateusz Matysiak <mateuszmatysiak96@gmail.com>
2026-02-10 15:18:52 +0100

    Add Cache-Control headers in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,46 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "jobs-api-ej8r.onrender.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "logo.clearbit.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  }
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "picsum.photos",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "jobs-api-ej8r.onrender.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "logo.clearbit.com",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
+	async headers() {
+		if (process.env.NODE_ENV !== "production") {
+			return [];
+		}
+
+		return [
+			{
+				source: "/isr",
+				headers: [
+					{
+						key: "Cache-Control",
+						value:
+							"public, max-age=600, s-maxage=3600, stale-while-revalidate=14400",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
