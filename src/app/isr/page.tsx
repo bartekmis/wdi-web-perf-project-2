6cafdb25c72c38ac69bea5931999d18d71cba693
kjabcon <kjabcon@intercars.eu>
2026-04-22 15:15:38 +0200

    cache strategy

--- src/app/isr/page.tsx
+++ src/app/isr/page.tsx
@@ -1,16 +1,21 @@
+/* src/app/isr/page.tsx */
+
 import { PerformanceMonitor } from "@/components/performance-monitor";
 import { getSectionISRContent } from "@/components/sections/section-isr";
 
-export default async function ISRPage() {
-  const { element, serverLoadTime, serverApiCallCount } = await getSectionISRContent();
+export const revalidate = 600;
 
-  return (
-    <>
-      {element}
-      <PerformanceMonitor
-        serverLoadTimes={{ isr: serverLoadTime }}
-        serverApiCalls={serverApiCallCount}
-      />
-    </>
-  );
+export default async function ISRPage() {
+	const { element, serverLoadTime, serverApiCallCount } =
+		await getSectionISRContent();
+
+	return (
+		<>
+			{element}
+			<PerformanceMonitor
+				serverLoadTimes={{ isr: serverLoadTime }}
+				serverApiCalls={serverApiCallCount}
+			/>
+		</>
+	);
 }
